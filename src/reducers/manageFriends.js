6bac7b7a3a7cea5bfd541759f26e3dfbf614d076
linkcoding <kenny.lin.91@gmail.com>
2017-10-04 15:24:22 -0400

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  switch(action.type) {
+    case 'ADD_FRIEND':
+      return {friends: [...state.friends, action.friend] }
+    case 'REMOVE_FRIEND':
+      return {friends: state.friends.filter(friend => friend.id !== action.id)}
+    default:
+      return state
+  }
 }
+
+// return {friends: state.friends.filter(friend => )}
+// return { friends: {...state, action.friend}}
